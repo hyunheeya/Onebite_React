5769747be005ecde78305e36302517e11f88886e
gusgml9718 <gusgml9718@knu.ac.kr>
2024-12-21 00:09:52 +0900

    3.5) Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -13,9 +13,12 @@
 
 
 
-
 // EMS
 import mul, { add, sub } from "./math.js"
-console.log(add(1, 2))   // 3
-console.log(sub(1, 2))   // -1
-console.log(mul(2, 3))   // 6
+// console.log(add(1, 2))   // 3
+// console.log(sub(1, 2))   // -1
+// console.log(mul(2, 3))   // 6
+
+import randomColor from "randomcolor"
+const color = randomColor()
+console.log(color)
