827e042e04811c24d06c9375be9c70e5cba976c7
gusgml9718 <gusgml9718@knu.ac.kr>
2024-12-24 18:13:14 +0900

    5.2) React 컴포넌트

---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,17 @@
+// function Header() {
+//   return (
+//     <header>
+//       <h1>header</h1>
+//     </header>
+//   )
+// }
+
+const Header = () => {
+    return (
+      <header>
+        <h1>header</h1>
+      </header>
+    )
+  }
+
+export default Header
